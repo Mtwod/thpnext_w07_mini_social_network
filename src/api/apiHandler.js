e352e8c3497f86f5cd7ea1a9b0a13206538179ae
Mtwod <maximewong@hotmail.com>
2021-02-16 16:28:34 +0100

    Add API handler

---
+++ src/api/apiHandler.js
@@ -0,0 +1,11 @@
+const REGISTER = {
+  URL: 'http://localhost:1337/auth/local/register',
+  METHOD: 'post',
+};
+
+const CONNEXION = {
+  URL: 'http://localhost:1337/auth/local',
+  METHOD: 'post',
+};
+
+export { REGISTER, CONNEXION };
