6ad5dd6e7cac897999fc751d1fa079da2336cdc1
Mtwod <maximewong@hotmail.com>
2021-02-17 19:12:04 +0100

    Fix fetch user from cookie token if present

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,66 @@
+import { OWN_PROFILE } from 'api/apiHandler';
 import Header from 'components/Header';
 import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Profile from 'pages/Profile';
 import Register from 'pages/Register';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from 'routes/PrivateRoute';
+import { fetchUserFromCookie } from 'store/user/userActions';
 import 'style.scss';
+import { checkAuthentication, getAuthenticationCookie } from 'utils/cookieUtils';
 
-const App = () => (
-  <div className="App">
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <PrivateRoute path="/profile" component={Profile} />
-      </Switch>
-    </Router>
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  const fetchCurrentUserFromCookie = async () => {
+    const token = getAuthenticationCookie();
+
+    const {
+      URL,
+      METHOD,
+      HEADERS,
+    } = OWN_PROFILE(token);
+
+    const response = await fetch(URL, {
+      method: METHOD,
+      headers: HEADERS,
+    });
+
+    const data = await response.json();
+
+    const { id, username } = data;
+
+    const user = { id, username };
+
+    dispatch(fetchUserFromCookie(user));
+  };
+
+  useEffect(() => {
+    if (checkAuthentication()) fetchCurrentUserFromCookie();
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,5 @@
-import { OWN_PROFILE } from 'api/apiHandler';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { checkAuthentication, getAuthenticationCookie } from 'utils/cookieUtils';
-import { fetchUserFromCookie } from './user/userActions';
 import registerReducer from './user/userReducer';
 
 const store = createStore(
@@ -13,38 +10,5 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
-
-const fetchCurrentUserFromCookie = async () => {
-  const token = getAuthenticationCookie();
-
-  const {
-    URL,
-    METHOD,
-    HEADERS,
-  } = OWN_PROFILE(token);
-
-  const response = await fetch(URL, {
-    method: METHOD,
-    headers: HEADERS,
-  });
-
-  const data = await response.json();
-
-  const {
-    username,
-    email,
-    description,
-  } = data;
-
-  const user = {
-    username,
-    email,
-    description,
-  };
-
-  store.dispatch(fetchUserFromCookie(user));
-};
-
-if (checkAuthentication()) fetchCurrentUserFromCookie();
 
 export default store;
